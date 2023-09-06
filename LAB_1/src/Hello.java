47678f98c60a99cbe0d31bafc93de8f62973782b
KIBA659 <89656344+KIBA659@users.noreply.github.com.>
2023-09-06 22:32:18 +0300

    Lab1

---
+++ LAB_1/src/Hello.java
@@ -0,0 +1,26 @@
+/**
+ * Клас Hello реалізує приклад програми до лабораторної роботи №1
+ * @author Olesko
+ * @version 1.0
+ * @since version 1.0
+ *
+ */
+
+public class Hello {
+
+	public static void main(String[] args) {
+		String message = new String();
+		String messages = new String();
+		/**
+		 * повідомлення 
+		 */
+		message = "Hello, Java";
+		messages = "Olesko Bohdan KI-306";
+		/**
+		 * вивід
+		 */
+		System.out.println(message);
+		System.out.println(messages);
+	}
+
+}
